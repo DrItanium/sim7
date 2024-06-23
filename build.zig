a393d80bf2402a9435f78f5260b90758fc31c221
Joshua Scoggins <Theoretically.x64@gmail.com>
2024-06-23 12:27:38 -0700

    Successsful simple build

--- build.zig
+++ build.zig
@@ -4,8 +4,8 @@ pub fn build(b: *std.Build) void {
     const exe = b.addExecutable(.{
         .name = "sim960",
         .root_source_file = .{ .path = "src/main.zig" },
-        .target = b.starndardTargetOptions(.{}),
-        .optimize = b.standardOptimizeOptions(.{}),
+        .target = b.standardTargetOptions(.{}),
+        .optimize = b.standardOptimizeOption(.{}),
     });
     b.installArtifact(exe);
 }
---
+++ src/main.zig
@@ -0,0 +1,5 @@
+const std = @import("std");
+
+pub fn main() void {
+    std.debug.print("i960 Simulator\n", .{});
+}
